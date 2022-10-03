a1f77a463a3509d4b4cc8c0ad07297c4509b6d3b
Bruno Barbosa <barbosa.bruno@me.com>
2022-10-03 15:12:03 -0300

    Add a horizontal layout to the CollectionView

---
+++ POCCompositionalLayout/Sources/Modules/Main/MainCollectionViewCell.swift
@@ -0,0 +1,67 @@
+//
+//  MainCollectionViewCell.swift
+//  POCCompositionalLayout
+//
+//  Created by Bruno Barbosa on 03/10/22.
+//
+
+import UIKit
+
+class MainCollectionViewCell: UICollectionViewCell {
+
+    // MARK: - Public properties
+
+    static let identifier = "main-collection-view-cell-reuse-identifier"
+
+    // MARK: - UI elements
+    
+    let label = UILabel()
+
+    // MARK: - Initialisers
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViewCode()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupViewCode()
+    }
+}
+
+extension MainCollectionViewCell {
+
+    private func setupViewCode() {
+        buildHierarchy()
+        setupConstraints()
+        configureViews()
+    }
+
+    private func buildHierarchy() {
+        contentView.addSubview(label)
+    }
+
+    private func setupConstraints() {
+        let inset = CGFloat(10)
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: inset),
+            label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -inset),
+            label.topAnchor.constraint(equalTo: contentView.topAnchor, constant: inset),
+            label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -inset)
+        ])
+    }
+
+    private func configureViews() {
+        contentView.backgroundColor = .gray
+        contentView.layer.borderColor = UIColor.black.cgColor
+        contentView.layer.borderWidth = 1
+        contentView.layer.cornerRadius = 8
+
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontForContentSizeCategory = true
+        label.font = UIFont.preferredFont(forTextStyle: .caption1)
+        label.textColor = .black
+        label.textAlignment = .center
+    }
+}
--- POCCompositionalLayout/Sources/Modules/Main/MainView.swift
+++ POCCompositionalLayout/Sources/Modules/Main/MainView.swift
@@ -11,45 +11,100 @@ class MainView: UIView {
 
     // MARK: - Private properties
 
-    private let mainLabel = UILabel()
+    private enum Section: CaseIterable {
+        case main
+    }
 
-    // MARK: - Initialization
+    private var dataSource: UICollectionViewDiffableDataSource<Int, Int>! = nil
+
+    private let collectionView = {
+        let collectionView = UICollectionView(frame: .zero,
+                                              collectionViewLayout: createCompositionalLayout())
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(MainCollectionViewCell.self,
+                                forCellWithReuseIdentifier: MainCollectionViewCell.identifier)
+        return collectionView
+    }()
+
+    // MARK: - Initializers
 
     override init(frame: CGRect) {
         super.init(frame: .zero)
         setupViewCode()
+        configureDataSource()
     }
 
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         setupViewCode()
+        configureDataSource()
+    }
+
+    // MARK: - Private methods
+
+    private static func createCompositionalLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5),
+                                              heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .fractionalWidth(0.5))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+
+        let section = NSCollectionLayoutSection(group: group)
+
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+
+    func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Int, Int>(collectionView: collectionView) {
+            (collectionView: UICollectionView,
+             indexPath: IndexPath, identifier: Int) -> UICollectionViewCell in
+
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MainCollectionViewCell.identifier,
+                                                                for: indexPath) as? MainCollectionViewCell else {                return UICollectionViewCell()
+            }
+
+            cell.label.text = "\(indexPath.section), \(indexPath.item)"
+            return cell
+        }
+
+        // initial data
+        var snapshot = NSDiffableDataSourceSnapshot<Int, Int>()
+        var identifierOffset = 0
+        let itemsPerSection = 4
+        for section in 0..<5 {
+            snapshot.appendSections([section])
+            let maxIdentifier = identifierOffset + itemsPerSection
+            snapshot.appendItems(Array(identifierOffset..<maxIdentifier))
+            identifierOffset += itemsPerSection
+        }
+        dataSource.apply(snapshot, animatingDifferences: false)
     }
 }
 
-// MARK: - View Code
-
 extension MainView {
+
+    // MARK: - View Code
 
     private func setupViewCode() {
         buildHierarchy()
         setupConstraints()
-        configureViews()
     }
 
     private func buildHierarchy() {
-        addSubview(mainLabel)
+        addSubview(collectionView)
     }
 
     private func setupConstraints() {
-        mainLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            mainLabel.centerXAnchor.constraint(equalTo: safeAreaLayoutGuide.centerXAnchor),
-            mainLabel.centerYAnchor.constraint(equalTo: safeAreaLayoutGuide.centerYAnchor)
+            collectionView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            collectionView.leftAnchor.constraint(equalTo: safeAreaLayoutGuide.leftAnchor),
+            collectionView.rightAnchor.constraint(equalTo: safeAreaLayoutGuide.rightAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
         ])
-    }
-
-    private func configureViews() {
-        mainLabel.text = "Hello World"
     }
 }
 
